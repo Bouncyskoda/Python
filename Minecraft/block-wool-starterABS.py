0a89b93c699f7a363a02888acc63c38386187d67
Bouncy Skoda <diamondminecraftadam638@gmail.com>
2021-01-15 12:40:31 -0800

    AA

--- Minecraft/block-wool-starterABS.py
+++ Minecraft/block-wool-starterABS.py
@@ -9,13 +9,14 @@ mc = Minecraft.create()
 mc.player.setPos(0,0,0)
 # CLEAR AN AREA WITH AIR TO BUILD
 air = 0
-mc.setBlocks(-24,0,-36,24,64,36,air) # clear some air                                               
+mc.setBlocks(-24,-1,-36,24,64,36,air) # clear some air                                               
 mc.player.setPos(0,0,0)
-for n in range (0,70):
+for n in range (0,54):
 
-	h = random.randint(0,10) #x
-	k = random.randint(0,10)	#y
-	l = random.randint(0,12)	#z
-	c = random.randint(1,15)	#color
+	h = random.randint(-1,1) #x
+	k = random.randint(-1,1)	#y
+	l = random.randint(-1,1)	#z
+	c = random.randint(0,5)	#color
 	mc.setBlock(h,k,l,35,c)
-	time.sleep(5)
+	time.sleep(0.25)
+mc.player.setPos(4,0,-4)
