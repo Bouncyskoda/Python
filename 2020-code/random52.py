262c423b5b987d3cf2f20891a0856d130b0efef6
Bouncy Skoda <diamondminecraftadam638@gmail.com>
2021-01-26 13:13:47 -0800

    AA

---
+++ 2020-code/random52.py
@@ -0,0 +1,14 @@
+import random
+for i in range (1,53): 
+ n =  random.randint(0,51)
+ print(n," ",end="")
+ if(i % 10 == 0):
+	 print(" * ")
+print("\n\t\t",i,"<- last i - - - \n\n")
+import random
+for i in range (1,53): 
+ n =  random.randint(0,51)
+ print(n," ",end="")
+ if(i % 10 == 0):
+	 print(" * ")
+print("\n\t\t",i,"<- last i - - - \n\n")
