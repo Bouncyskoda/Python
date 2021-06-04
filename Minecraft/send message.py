103657a9ceca41376a41295a1c446d6aa4144979
Bouncy Skoda <diamondminecraftadam638@gmail.com>
2021-06-04 13:27:32 -0700

    AA

--- Minecraft/send message.py
+++ Minecraft/send message.py
@@ -2,22 +2,8 @@ from mcpi.minecraft import Minecraft
 from mcpi import block	  
 
 
-ip = "10.183.3.40"
+ip = "10.183.0.23"
 
 mc = Minecraft.create(ip, 4711)
 
-mc.postToChat("secret-account joined the game")
-                                                 
-
-
-
-#COAL_ORE             16
-#WOOD                 17
-#LEAVES               18
-#Crafting Table       58
-#furnace              61
-#Fence				  85
-#Bedrock			   7
-#Wool				  35
-#Bookshelf			  47
-#Glowstone			  89
+mc.postToChat("the city is doom commander fist fight I've posioned the water supply")
